3772e94ad332cf851aebd58772aed8dacbac955d
devisinghlodhi <devisinghlodhi1999@gmail.com>
2022-07-07 16:45:31 +0530

    complete

--- Controllers/executeSwift.js
+++ Controllers/executeSwift.js
@@ -14,7 +14,7 @@ const executeSwift = (filepath)=>{
     return new Promise((resolve, reject)=>{
         
         // exec(`docker exec -i --user normaluser ${ConId} swift ${jobId}/${jobId}.swift `, (error, stdout, stderr) => {
-        exec(`cd ${codeJobidfolderPath} && swift ${jobId}.swift`, (error, stdout, stderr)=>{
+        exec(`swift ${filepath}`, (error, stdout, stderr)=>{
             if(stderr){
                 deletefiles([filepath]);
                 deletefolders([codeJobidfolderPath]);
